6a3cba8d41946e96ed1df8fca12e042017749d65
= <darsh.astro@outlook.com>
2025-05-30 15:43:38 +0530

    added the modified files

---
+++ SupervisedLearning/Regression/LogisticRegression/LogisticRegression.py
@@ -0,0 +1,7 @@
+import seaborn as sns 
+import pandas as pd 
+import numpy as np 
+
+dataframe = pd.read_csv("SupervisedLearning/Regression/LogisticRegression/diabetes.csv")
+print(dataframe.head())
+
