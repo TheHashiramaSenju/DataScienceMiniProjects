0eb733843ba663cb6d336b2a5897f306f71a498f
= <darsh.astro@outlook.com>
2025-05-30 16:03:30 +0530

    added files of readme and logistic Regression init

--- SupervisedLearning/Regression/LogisticRegression/LogisticRegression.py
+++ SupervisedLearning/Regression/LogisticRegression/LogisticRegression.py
@@ -1,7 +1,22 @@
 import seaborn as sns 
 import pandas as pd 
 import numpy as np 
+import matplotlib.pyplot as plt
 
 dataframe = pd.read_csv("SupervisedLearning/Regression/LogisticRegression/diabetes.csv")
-print(dataframe.head())
+print(dataframe.head(20)) #we observe the data-structures here
+print()
+ 
+print(dataframe.shape) #observe the values and the types 
+print() 
 
+print(dataframe.corr)
+print()
+
+a = plt.figure(figsize=(15,15))
+print(a)
+print()
+
+ax = sns.heatmap(dataframe.corr(), annot=True)
+plt.savefig('correlation-coeff.jpg')
+plt.show()
